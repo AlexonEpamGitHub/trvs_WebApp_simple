f442f79818b0f4a82fe29ea82c9845db1e3d3bc9
Oleksandr Onyshchenko <Oleksandr_Onyshchenko@epam.com>
2025-05-13 15:09:09 +0300

    created

---
+++ WebAppResult_core8/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppResult_core8.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
