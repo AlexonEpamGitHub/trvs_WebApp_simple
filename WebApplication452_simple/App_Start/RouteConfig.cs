ff7eba838f332632acb49a032f770fb3e9467916
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-14 02:15:06 -0700

    Replace System.Web.Mvc and System.Web.Routing with Microsoft.AspNetCore.Routing. Update RouteConfig to use IEndpointRouteBuilder for .NET Core.

--- WebApplication452_simple/App_Start/RouteConfig.cs
+++ WebApplication452_simple/App_Start/RouteConfig.cs
@@ -1,23 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.Routing;
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
 
 namespace WebApplication452_simple
 {
     public class RouteConfig
     {
-        public static void RegisterRoutes(RouteCollection routes)
+        public static void RegisterRoutes(IEndpointRouteBuilder endpoints)
         {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
         }
     }
 }
