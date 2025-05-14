c8444f7e556d9112a00a2f2283bfe9355a260aa9
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-14 04:26:27 -0700

    Update HomeController for ASP.NET Core MVC compatibility.

--- WebApplication452_simple/Controllers/HomeController.cs
+++ WebApplication452_simple/Controllers/HomeController.cs
@@ -12,14 +12,12 @@ namespace WebApplication452_simple.Controllers
         public IActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
         public IActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
