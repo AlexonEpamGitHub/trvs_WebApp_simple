f32eca272335b7c302fff64d003a736d1d299501
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-14 00:12:45 -0700

    Refactor HomeController.cs to align with ASP.NET Core MVC conventions.

--- WebApplication452_simple/Controllers/HomeController.cs
+++ WebApplication452_simple/Controllers/HomeController.cs
@@ -5,24 +5,26 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WebApplication452_simple.Controllers
 {
+    [Route("[controller]")]
     public class HomeController : Controller
     {
+        [HttpGet("")]
         public IActionResult Index()
         {
             return View();
         }
 
+        [HttpGet("about")]
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
-
             return View();
         }
 
+        [HttpGet("contact")]
         public IActionResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
-
             return View();
         }
     }
