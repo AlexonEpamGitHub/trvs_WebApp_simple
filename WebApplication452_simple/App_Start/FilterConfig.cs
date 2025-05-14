47bc917af7c5451695a584e09bb42d309b2e6462
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-14 02:15:08 -0700

    Update FilterConfig to use IServiceCollection for .NET Core

--- WebApplication452_simple/App_Start/FilterConfig.cs
+++ WebApplication452_simple/App_Start/FilterConfig.cs
@@ -1,13 +1,29 @@
-﻿using System.Web;
-using System.Web.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace WebApplication452_simple
 {
     public class FilterConfig
     {
-        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        public static void RegisterGlobalFilters(IServiceCollection services)
         {
-            filters.Add(new HandleErrorAttribute());
+            services.AddControllers(options =>
+            {
+                options.Filters.Add(new HandleErrorAttribute());
+            });
+        }
+    }
+
+    public class HandleErrorAttribute : ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            // Handle the exception and set the result.
+            context.ExceptionHandled = true;
+            context.Result = new Microsoft.AspNetCore.Mvc.ViewResult 
+            { 
+                ViewName = "Error"
+            };
         }
     }
 }
