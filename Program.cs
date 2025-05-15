e08f347d9f39e25347c7cb63469c8b4dcf91ba83
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-15 06:25:51 -0700

    Migrate route configuration from RouteConfig.cs to Program.cs.

--- Program.cs
+++ Program.cs
@@ -23,8 +23,29 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+// Define routing configuration
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
+    name: "blog",
+    pattern: "blog/{articleId}",
+    defaults: new { controller = "Blog", action = "Article" });
+
+app.MapControllerRoute(
+    name: "categories",
+    pattern: "categories/{categoryName}/{pageNumber?}",
+    defaults: new { controller = "Category", action = "List" });
+
+app.MapControllerRoute(
+    name: "product",
+    pattern: "product/{productId}/{action=Details}",
+    defaults: new { controller = "Product" });
+
+app.MapControllerRoute(
+    name: "admin",
+    pattern: "admin/{action=Dashboard}/{id?}",
+    defaults: new { controller = "Admin" });
 
 app.Run();
