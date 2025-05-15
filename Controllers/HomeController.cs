f34a71d8bd956e8ba5b65aaf14007c2f7ccae2e5
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-15 04:08:35 -0700

    Recreated HomeController.cs with .NET 8 standards and Microsoft.AspNetCore.Mvc conventions.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication452_simple.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
