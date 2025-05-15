89217efb233e9054b8a9f4d99d8c1db0bfbdd801
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-15 06:13:25 -0700

    Migrate global filter configuration to Program.cs and set up .NET Core/8 application pipeline.

--- WebApplication452_simple/Program.cs
+++ WebApplication452_simple/Program.cs
@@ -1,11 +1,36 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add(new HandleErrorAttribute());
+});
+
 var app = builder.Build();
 
+// Configure the HTTP request pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
 app.UseRouting();
+
+app.UseAuthorization();
 
 app.UseEndpoints(endpoints =>
 {
