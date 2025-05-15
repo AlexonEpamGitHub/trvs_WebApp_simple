75607b587c548f89af96f47f712fcd8521261a09
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-15 04:29:11 -0700

    Create Program.cs for ASP.NET Core routing migration

---
+++ WebApplication452_simple/Program.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+});
+
+app.Run();
