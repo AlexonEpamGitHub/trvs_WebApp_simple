ac62c8b11957b9ed1dd2cc46483aad76d74790b9
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-16 02:55:15 -0700

    Updated WebApplication452_simple/Controllers/HomeController.cs for .NET 8 migration: changed namespace, replaced System.Web.Mvc with Microsoft.AspNetCore.Mvc, updated base class and IActionResult.

--- WebApplication452_simple/Controllers/HomeController.cs
+++ WebApplication452_simple/Controllers/HomeController.cs
@@ -1,26 +1,25 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+using Microsoft.AspNetCore.Mvc;
 
-namespace WebApplication452_simple.Controllers
+namespace WebApplication8.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : ControllerBase
     {
-        public ActionResult Index()
+        public IActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public IActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
-        public ActionResult Contact()
+        public IActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
