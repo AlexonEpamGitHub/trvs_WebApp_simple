c3eec89d19351696de0e966b5e62275bf6f44514
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-16 00:40:31 -0700

    Updated HomeController namespace and logic for .NET 8 migration

--- WebApplication452_simple/Controllers/HomeController.cs
+++ WebApplication452_simple/Controllers/HomeController.cs
@@ -1,28 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebApplication452_simple.Controllers
 {
+    [Route("[controller]")]
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        [HttpGet("")]
+        public IActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        [HttpGet("about")]
+        public IActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewData["Message"] = "Your application description page.";
 
             return View();
         }
 
-        public ActionResult Contact()
+        [HttpGet("contact")]
+        public IActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewData["Message"] = "Your contact page.";
 
             return View();
         }
