2c5d24752e72caa2685c9c12b57467df6fceb0cf
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-15 23:38:41 -0700

    Create Program.cs to serve as the application's entry point and configure the WebApplication class.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
