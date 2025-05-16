ce0075425a229cb4c4ed9bf41e9b7ff0e0b8f4aa
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-16 03:15:45 -0700

    QA: Migrate routing logic to Program.cs and mark RouteConfig.cs for deletion

--- WebApplication452_simple/App_Start/RouteConfig.cs
+++ WebApplication452_simple/App_Start/RouteConfig.cs
@@ -1,23 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.Routing;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace WebApplication452_simple
 {
-    public class RouteConfig
+    public class Program
     {
-        public static void RegisterRoutes(RouteCollection routes)
+        public static void Main(string[] args)
         {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            var host = CreateHostBuilder(args).Build();
+            host.Run();
+        }
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
