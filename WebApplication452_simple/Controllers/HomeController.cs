45e10a64772e9936454cd6bd31fa2811cb91f165
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-16 03:15:24 -0700

    QA: Updated HomeController.cs for .NET Core migration

--- WebApplication452_simple/Controllers/HomeController.cs
+++ WebApplication452_simple/Controllers/HomeController.cs
@@ -12,14 +12,12 @@ namespace WebApplication452_simple.Controllers
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
-
             return View();
         }
 
         public IActionResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
-
             return View();
         }
     }
