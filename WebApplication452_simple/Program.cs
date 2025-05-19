83a82e9d5dd354b56889d207624eade1eb0d88e0
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-19 08:42:15 -0700

    QA: Migrate global filter registration to Program.cs

--- WebApplication452_simple/Program.cs
+++ WebApplication452_simple/Program.cs
@@ -1,12 +1,16 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Register MVC services
-builder.Services.AddControllersWithViews();
+// Register MVC services with global filter configuration
+builder.Services.AddControllersWithViews(options =>
+{
+    options.Filters.Add(new Microsoft.AspNetCore.Mvc.Filters.ExceptionFilterAttribute());
+});
 
 var app = builder.Build();
 
