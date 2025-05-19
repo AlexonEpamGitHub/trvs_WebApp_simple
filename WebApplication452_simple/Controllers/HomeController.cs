e2cb0e1cc75883a16e20e078d3b463d6eb7836f6
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-19 04:36:34 -0700

    QA: Updated HomeController for ASP.NET Core MVC conventions and compatibility.

--- WebApplication452_simple/Controllers/HomeController.cs
+++ WebApplication452_simple/Controllers/HomeController.cs
@@ -6,17 +6,20 @@ namespace WebApplication452_simple.Controllers
     {
         public IActionResult Index()
         {
+            // Returning the view using ASP.NET Core MVC conventions
             return View();
         }
 
         public IActionResult About()
         {
+            // Using ViewData to store application description message
             ViewData["Message"] = "Your application description page.";
             return View();
         }
 
         public IActionResult Contact()
         {
+            // Using ViewData to store contact page message
             ViewData["Message"] = "Your contact page.";
             return View();
         }
