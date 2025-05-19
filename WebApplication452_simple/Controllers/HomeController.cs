741b1938a835cd6f9f77e6fdfb5a76a8495d0366
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-19 08:34:40 -0700

    Updated HomeController to migrate from System.Web.Mvc to Microsoft.AspNetCore.Mvc, replaced ActionResult with IActionResult, replaced ViewBag with ViewData, and implemented Task-based async patterns.

--- WebApplication452_simple/Controllers/HomeController.cs
+++ WebApplication452_simple/Controllers/HomeController.cs
@@ -1,28 +1,28 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebApplication452_simple.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public async Task<IActionResult> Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public async Task<IActionResult> About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewData["Message"] = "Your application description page.";
 
             return View();
         }
 
-        public ActionResult Contact()
+        public async Task<IActionResult> Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewData["Message"] = "Your contact page.";
 
             return View();
         }
