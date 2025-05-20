79f5ab9217cd33b5ce0a2f5fb511a1c7be46abc2
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-20 09:16:33 -0700

    Create Program.cs to serve as the new entry point for ASP.NET Core and include global filter configuration.

---
+++ WebApplication452_simple/Program.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+builder.Services.AddControllersWithViews(options =>
+{
+    // Add global filters equivalent to HandleErrorAttribute
+    options.Filters.Add(new Microsoft.AspNetCore.Mvc.Filters.ExceptionFilterAttribute());
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+});
+
+app.Run();
