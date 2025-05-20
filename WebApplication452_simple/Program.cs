758a83d45620ee9587e5b57bafce33a5ed14b024
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-20 07:24:46 -0700

    Update Program.cs with middleware for static files and routing for .NET 8 migration.

--- WebApplication452_simple/Program.cs
+++ WebApplication452_simple/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Http;
+using System.IO;
 
 namespace WebApplication452_simple
 {
@@ -42,6 +44,48 @@ namespace WebApplication452_simple
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+
+            // Middleware to handle CSS and JavaScript bundling
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path.StartsWithSegments("/bundles"))
+                {
+                    var bundleName = context.Request.Path.Value.Substring("/bundles".Length).TrimStart('/').ToLower();
+
+                    if (bundleName == "styles")
+                    {
+                        context.Response.ContentType = "text/css";
+                        await context.Response.WriteAsync("/* Bundled CSS */\n");
+
+                        var cssFiles = new[] { "wwwroot/css/site.css" };
+                        foreach (var file in cssFiles)
+                        {
+                            if (File.Exists(file))
+                            {
+                                await context.Response.WriteAsync(await File.ReadAllTextAsync(file));
+                            }
+                        }
+                        return;
+                    }
+                    else if (bundleName == "scripts")
+                    {
+                        context.Response.ContentType = "application/javascript";
+                        await context.Response.WriteAsync("// Bundled JavaScript\n");
+
+                        var jsFiles = new[] { "wwwroot/js/site.js" };
+                        foreach (var file in jsFiles)
+                        {
+                            if (File.Exists(file))
+                            {
+                                await context.Response.WriteAsync(await File.ReadAllTextAsync(file));
+                            }
+                        }
+                        return;
+                    }
+                }
+
+                await next();
+            });
 
             app.UseRouting();
 
