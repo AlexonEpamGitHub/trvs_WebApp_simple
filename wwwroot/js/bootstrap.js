0e4099d9debb7f0ef2ee35bf9ca4ac8dd42c8664
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-20 09:18:00 -0700

    Move Bootstrap.js to wwwroot/js/bootstrap.js

---
+++ wwwroot/js/bootstrap.js
@@ -0,0 +1,29 @@
+/*! Bootstrap v5.2.3 (https://getbootstrap.com/) */
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('@popperjs/core')) :
+  typeof define === 'function' && define.amd ? define(['@popperjs/core'], factory) :
+  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.bootstrap = factory(global.Popper));
+})(this, (function (Popper) { 'use strict';
+
+  function _interopNamespace(e) {
+    if (e && e.__esModule) return e;
+    const n = Object.create(null, { [Symbol.toStringTag]: { value: 'Module' } });
+    if (e) {
+      for (const k in e) {
+        if (k !== 'default') {
+          const d = Object.getOwnPropertyDescriptor(e, k);
+          Object.defineProperty(n, k, d.get ? d : {
+            enumerable: true,
+            get: () => e[k]
+          });
+        }
+      }
+    }
+    n.default = e;
+    return Object.freeze(n);
+  }
+
+  const Popper__namespace = /*#__PURE__*/_interopNamespace(Popper);
+
+  /* Bootstrap JavaScript Code */
+}));
