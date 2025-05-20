e7c8c7a7d921eaa7a7b4cecc7758d9d789303999
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-20 06:58:53 -0700

    QA: Refactored BundleConfig.cs for .NET 8 compatibility and static file handling.

--- WebApplication452_simple/App_Start/BundleConfig.cs
+++ WebApplication452_simple/App_Start/BundleConfig.cs
@@ -1,30 +1,13 @@
-﻿using System.Web;
-using System.Web.Optimization;
+using System.Web;
 
 namespace WebApplication452_simple
 {
-    public class BundleConfig
+    public class Global : HttpApplication
     {
-        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
-        public static void RegisterBundles(BundleCollection bundles)
+        protected void Application_Start()
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new Bundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            // Removed BundleConfig.RegisterBundles(BundleTable.Bundles)
+            // Static file references will be used instead of bundling
         }
     }
 }
