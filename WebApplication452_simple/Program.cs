d3e303a662b1c5c5f76ae42429fd74ae98c93999
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-20 05:27:55 -0700

    Migrate startup logic from Global.asax.cs to Program.cs following .NET 8 standards.

--- WebApplication452_simple/Program.cs
+++ WebApplication452_simple/Program.cs
@@ -1,10 +1,24 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews(options =>
+{
+    // Register global filters
+    options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
+});
+
+// Area registration
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+builder.Services.Configure<MvcOptions>(options =>
+{
+    options.Conventions.Add(new AreaAttributeRouteModelConvention());
+});
 
 var app = builder.Build();
 
@@ -21,6 +35,11 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+// Area registration and routing
+app.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 
 app.MapControllerRoute(
     name: "default",
