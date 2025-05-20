4d0b03c0ca18f9eb26c30a18b44548e197b65a35
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-20 06:06:48 -0700

    Migrated route definitions to Program.cs using MapControllerRoute method for .NET Core compatibility.

--- WebApplication452_simple/App_Start/RouteConfig.cs
+++ WebApplication452_simple/App_Start/RouteConfig.cs
@@ -1,23 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.Routing;
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace WebApplication452_simple
 {
-    public class RouteConfig
+    public class Program
     {
-        public static void RegisterRoutes(RouteCollection routes)
+        public static void Main(string[] args)
         {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            var builder = WebApplication.CreateBuilder(args);
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
         }
     }
 }
