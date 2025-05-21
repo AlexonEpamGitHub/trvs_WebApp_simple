df30fd4e3bcc8f268ae0817cdf7d1293c9d762e0
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-21 06:39:56 -0700

    QA: filterconfig_cs--refactored-global-filters

--- WebApplication452_simple/App_Start/FilterConfig.cs
+++ WebApplication452_simple/App_Start/FilterConfig.cs
@@ -3,13 +3,24 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace WebApplication452_simple
 {
+    public class HandleErrorFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            // Custom error handling logic can be implemented here.
+            // For demonstration purposes, this is left empty.
+        }
+    }
+
     public static class FilterConfig
     {
         public static void RegisterGlobalFilters(IServiceCollection services)
         {
+            services.AddScoped<HandleErrorFilter>();
+
             services.AddControllersWithViews(options =>
             {
-                options.Filters.Add(new HandleErrorAttribute());
+                options.Filters.AddService<HandleErrorFilter>();
             });
         }
     }
