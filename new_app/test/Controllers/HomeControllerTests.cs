cd9fea96894a98cce3bbc3eeb02b58bb166cfe37
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-21 06:53:48 -0700

    homecontroller-tests--added-unit-tests-for-actions

---
+++ new_app/test/Controllers/HomeControllerTests.cs
@@ -0,0 +1,45 @@
+using Xunit;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using new_app.Controllers;
+
+namespace new_app.test.Controllers
+{
+    public class HomeControllerTests
+    {
+        private readonly HomeController _controller;
+
+        public HomeControllerTests()
+        {
+            // Initialize HomeController for testing.
+            _controller = new HomeController();
+        }
+
+        [Fact]
+        public void Index_ReturnsViewResult_WithCorrectViewName()
+        {
+            var result = _controller.Index() as ViewResult;
+
+            Assert.NotNull(result);
+            Assert.Equal("Index", result?.ViewName);
+        }
+
+        [Fact]
+        public void About_ReturnsViewResult_WithCorrectViewName()
+        {
+            var result = _controller.About() as ViewResult;
+
+            Assert.NotNull(result);
+            Assert.Equal("About", result?.ViewName);
+        }
+
+        [Fact]
+        public void Contact_ReturnsViewResult_WithCorrectViewName()
+        {
+            var result = _controller.Contact() as ViewResult;
+
+            Assert.NotNull(result);
+            Assert.Equal("Contact", result?.ViewName);
+        }
+    }
+}
