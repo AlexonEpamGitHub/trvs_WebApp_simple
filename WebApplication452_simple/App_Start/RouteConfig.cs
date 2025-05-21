49a1e0c08e02027f12c9832a5f13505594dc8aa3
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-21 04:52:12 -0700

    QA: routeconfig_cs--modern-endpoint-routing

--- WebApplication452_simple/App_Start/RouteConfig.cs
+++ WebApplication452_simple/App_Start/RouteConfig.cs
@@ -1,23 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.Routing;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
 
 namespace WebApplication452_simple
 {
     public class RouteConfig
     {
-        public static void RegisterRoutes(RouteCollection routes)
+        public static void ConfigureRoutes(IEndpointRouteBuilder endpoints)
         {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-            routes.MapRoute(
+            endpoints.MapControllerRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                pattern: "{controller=Home}/{action=Index}/{id?}");
         }
     }
 }
