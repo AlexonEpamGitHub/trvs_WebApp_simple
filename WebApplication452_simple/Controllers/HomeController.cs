a6550ad705805acd38d42808159d9a2b26df549f
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-21 04:52:44 -0700

    QA: homecontroller_cs--refactored-to-aspnetcore

--- WebApplication452_simple/Controllers/HomeController.cs
+++ WebApplication452_simple/Controllers/HomeController.cs
@@ -1,24 +1,81 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace WebApplication452_simple.Controllers
 {
     public class HomeController : Controller
     {
-        public async Task<IActionResult> Index()
+        private readonly IHomeService _homeService;
+
+        public HomeController(IHomeService homeService)
         {
-            return await Task.FromResult(View());
+            _homeService = homeService;
         }
 
-        public async Task<IActionResult> About()
+        public IActionResult Index()
         {
-            ViewBag.Message = "Your application description page.";
-            return await Task.FromResult(View());
+            var model = _homeService.GetIndexModel();
+            return View(model);
         }
 
-        public async Task<IActionResult> Contact()
+        public IActionResult About()
         {
-            ViewBag.Message = "Your contact page.";
-            return await Task.FromResult(View());
+            var model = _homeService.GetAboutModel();
+            ViewBag.Message = model.Message;
+            return View(model);
         }
+
+        public IActionResult Contact()
+        {
+            var model = _homeService.GetContactModel();
+            ViewBag.Message = model.Message;
+            return View(model);
+        }
+    }
+
+    public interface IHomeService
+    {
+        IndexViewModel GetIndexModel();
+        AboutViewModel GetAboutModel();
+        ContactViewModel GetContactModel();
+    }
+
+    public class HomeService : IHomeService
+    {
+        public IndexViewModel GetIndexModel()
+        {
+            return new IndexViewModel();
+        }
+
+        public AboutViewModel GetAboutModel()
+        {
+            return new AboutViewModel
+            {
+                Message = "Your application description page."
+            };
+        }
+
+        public ContactViewModel GetContactModel()
+        {
+            return new ContactViewModel
+            {
+                Message = "Your contact page."
+            };
+        }
+    }
+
+    public class IndexViewModel
+    {
+        // Add properties as needed for the Index view
+    }
+
+    public class AboutViewModel
+    {
+        public string Message { get; set; }
+    }
+
+    public class ContactViewModel
+    {
+        public string Message { get; set; }
     }
 }
