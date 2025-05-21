38eefcd46a0e0d131b2b3f0fe8b14729fd8d9c87
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-21 06:17:58 -0700

    bundleconfig--refactored-for-net-core

--- WebApplication452_simple/App_Start/BundleConfig.cs
+++ WebApplication452_simple/App_Start/BundleConfig.cs
@@ -1,30 +1,54 @@
-﻿using System.Web;
-using System.Web.Optimization;
+using Microsoft.Extensions.FileProviders;
+using System.IO;
 
 namespace WebApplication452_simple
 {
-    public class BundleConfig
+    public class Startup
     {
-        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
-        public static void RegisterBundles(BundleCollection bundles)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            // Serve static files from the wwwroot folder
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(
+                    Path.Combine(env.ContentRootPath, "wwwroot")),
+                RequestPath = ""
+            });
 
-            bundles.Add(new Bundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+            app.UseRouting();
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
         }
     }
+
+    // Note to developers:
+    // Bundling and minification of assets like JavaScript and CSS should be handled using modern tools
+    // such as Webpack, Gulp, or similar. These tools provide better performance and flexibility.
+    // Place your static files in the "wwwroot" folder, which is the default location for serving static files in .NET Core.
+    // Refer to the official documentation for guidance on using these tools effectively.
 }
