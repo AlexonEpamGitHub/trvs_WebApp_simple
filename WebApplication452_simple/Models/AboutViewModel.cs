47c39daef9fa2d897c47cb1260efb6715e6a6919
AlexonEpamGitHub <135996907+AlexonEpamGitHub@users.noreply.github.com>
2025-05-21 11:15:26 +0300

    QA: Create AboutViewModel.cs for strongly typed model in About.cshtml

---
+++ WebApplication452_simple/Models/AboutViewModel.cs
@@ -0,0 +1,8 @@
+namespace WebApplication452_simple.Models
+{
+    public class AboutViewModel
+    {
+        public string Title { get; set; }
+        public string Message { get; set; }
+    }
+}
