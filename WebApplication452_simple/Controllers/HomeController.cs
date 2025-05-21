8507d7aeb2aa5d7e95abdb737737c9f662241576
AlexonEpamGitHub <135996907+AlexonEpamGitHub@users.noreply.github.com>
2025-05-21 11:15:21 +0300

    QA: Updated HomeController to use Microsoft.AspNetCore.Mvc and asynchronous IActionResult responses.

--- WebApplication452_simple/Controllers/HomeController.cs
+++ WebApplication452_simple/Controllers/HomeController.cs
@@ -1,30 +1,30 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebApplication452_simple.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            return await Task.FromResult(View());
         }
 
-        public ActionResult About()
+        public async Task<IActionResult> About()
         {
             ViewBag.Message = "Your application description page.";
 
-            return View();
+            return await Task.FromResult(View());
         }
 
-        public ActionResult Contact()
+        public async Task<IActionResult> Contact()
         {
             ViewBag.Message = "Your contact page.";
 
-            return View();
+            return await Task.FromResult(View());
         }
     }
 }
