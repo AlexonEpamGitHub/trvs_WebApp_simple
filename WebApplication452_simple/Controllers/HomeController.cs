4ea25fed26b36961b7355f07ba0cf37f05df2b64
AlexonEpamGitHub <135996907+AlexonEpamGitHub@users.noreply.github.com>
2025-05-21 11:15:38 +0300

    QA: Add strongly typed model support for About.cshtml in HomeController.

--- WebApplication452_simple/Controllers/HomeController.cs
+++ WebApplication452_simple/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebApplication452_simple.Models;
 
 namespace WebApplication452_simple.Controllers
 {
@@ -13,11 +14,14 @@ namespace WebApplication452_simple.Controllers
             return await Task.FromResult(View());
         }
 
-        public async Task<IActionResult> About()
+        public IActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return await Task.FromResult(View());
+            var model = new AboutViewModel
+            {
+                Title = "About",
+                Message = "Use this area to provide additional information."
+            };
+            return View(model);
         }
 
         public async Task<IActionResult> Contact()
@@ -26,5 +30,14 @@ namespace WebApplication452_simple.Controllers
 
             return await Task.FromResult(View());
         }
+    }
+}
+
+namespace WebApplication452_simple.Models
+{
+    public class AboutViewModel
+    {
+        public string Title { get; set; }
+        public string Message { get; set; }
     }
 }
