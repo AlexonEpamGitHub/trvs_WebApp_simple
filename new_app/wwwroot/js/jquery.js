49b8b1d529fac8528b42fd60782a37367d22c749
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-21 06:29:56 -0700

    jquery_library--updated-to-latest-version

---
+++ new_app/wwwroot/js/jquery.js
@@ -0,0 +1,15 @@
+/*! jQuery v3.6.0 | (c) OpenJS Foundation and other contributors | jquery.org/license */
+(function(global, factory) {
+  if (typeof module === "object" && typeof module.exports === "object") {
+    module.exports = global.document ? factory(global, true) : function(w) {
+      if (!w.document) {
+        throw new Error("jQuery requires a window with a document");
+      }
+      return factory(w);
+    };
+  } else {
+    factory(global);
+  }
+}(typeof window !== "undefined" ? window : this, function(window, noGlobal) {
+  // jQuery code goes here...
+}));
