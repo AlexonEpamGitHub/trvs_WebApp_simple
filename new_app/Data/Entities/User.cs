298af0b33a944246f50baa192c52bcf6badb92a2
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-21 03:49:34 -0700

    user_entity--added-password-hashing

--- new_app/Data/Entities/User.cs
+++ new_app/Data/Entities/User.cs
@@ -1,5 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace new_app.Data.Entities
 {
@@ -22,6 +24,30 @@ namespace new_app.Data.Entities
         public string Email { get; set; }
 
         [Required]
-        public string PasswordHash { get; set; }
+        public string PasswordHash { get; private set; }
+
+        public void SetPassword(string password)
+        {
+            PasswordHash = HashPassword(password);
+        }
+
+        private static string HashPassword(string password)
+        {
+            using (var rfc2898 = new Rfc2898DeriveBytes(password, GenerateSalt(), 10000, HashAlgorithmName.SHA256))
+            {
+                byte[] hash = rfc2898.GetBytes(32);
+                return Convert.ToBase64String(hash);
+            }
+        }
+
+        private static byte[] GenerateSalt()
+        {
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                byte[] salt = new byte[16];
+                rng.GetBytes(salt);
+                return salt;
+            }
+        }
     }
 }
