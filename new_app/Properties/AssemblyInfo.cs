c1d6c132ed7757cfa20ef04737de0609c4d2d791
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-21 06:27:36 -0700

    assemblyinfo--modernized-metadata

---
+++ new_app/Properties/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+// Modernized AssemblyInfo.cs for .NET 8 migration
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WebApplication")]
+[assembly: AssemblyDescription("Modernized ASP.NET Core MVC Application")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("YourCompany")]
+[assembly: AssemblyProduct("WebApplication Product Suite")]
+[assembly: AssemblyCopyright("Copyright © 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible is false to make types in this assembly not visible 
+// to COM components. If this assembly is needed by COM, set ComVisible to true.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d2c44c55-9d5f-4c9a-b8b8-123456789abc")]
+
+// Version information for an assembly consists of the following four values:
+// Major Version
+// Minor Version 
+// Build Number
+// Revision
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
