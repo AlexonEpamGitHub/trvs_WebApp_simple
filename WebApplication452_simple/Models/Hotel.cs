b7d8b09ceb5e02c2ae399ddbda7fe6ec1adea9ae
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-23 04:31:03 -0700

    hotel_model--modernized-ef

--- WebApplication452_simple/Models/Hotel.cs
+++ WebApplication452_simple/Models/Hotel.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
-using WebApplication452_simple.Models;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace WebApplication452_simple.Models
+namespace WebApplication.Models
 {
     public class Hotel
     {
@@ -15,23 +11,23 @@ namespace WebApplication452_simple.Models
         [MaxLength(255)]
         public string Name { get; set; }
 
-        public Country Country { get; set; }
-
-        [Required]
-        [Display(Name = "Country")]
+        [ForeignKey(nameof(Country))]
         public int CountryId { get; set; }
+
+        public Country Country { get; set; }
 
         [Required]
         [MaxLength(50)]
         public string City { get; set; }
 
         [Required]
-        [Range(1,5)]
+        [Range(1, 5)]
         public int Stars { get; set; }
 
         [Required]
+        [Column(TypeName = "decimal(18,2)")]
         [Range(1, 1000)]
-        public double PricePerNight { get; set; }
+        public decimal PricePerNight { get; set; }
 
         [Required]
         public bool IsAllInclusive { get; set; }
