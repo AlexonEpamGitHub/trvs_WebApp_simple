759048ab73397d54b6c26c4e5d9c654fc5ab44c0
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-23 04:31:32 -0700

    models_customer--added-customer-entity

---
+++ new_app/Models/Customer.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace new_app.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+
+        public ICollection<Order> Orders { get; set; } = new List<Order>();
+
+        [NotMapped]
+        public int Age => DateTime.Now.Year - DateOfBirth.Year - (DateTime.Now.DayOfYear < DateOfBirth.DayOfYear ? 1 : 0);
+    }
+}
