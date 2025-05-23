d05d641e36bcbc1edae8c1940b4f18ed56db7d6e
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-23 04:26:42 -0700

    entities--modernize-hotel-refactor

--- WebApplication452_simple/Models/Hotel.cs
+++ WebApplication452_simple/Models/Hotel.cs
@@ -1,37 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
-using WebApplication452_simple.Models;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebApplication452_simple.Models
 {
     public class Hotel
     {
+        [Key]
         public int Id { get; set; }
 
         [Required]
         [MaxLength(255)]
         public string Name { get; set; }
 
-        public Country Country { get; set; }
-
-        [Required]
-        [Display(Name = "Country")]
+        [ForeignKey(nameof(Country))]
         public int CountryId { get; set; }
+
+        public Country Country { get; set; }
 
         [Required]
         [MaxLength(50)]
         public string City { get; set; }
 
         [Required]
-        [Range(1,5)]
+        [Range(1, 5)]
         public int Stars { get; set; }
 
         [Required]
-        [Range(1, 1000)]
-        public double PricePerNight { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        [Range(1, 1000, ErrorMessage = "Price per night must be between 1 and 1000.")]
+        public decimal PricePerNight { get; set; }
 
         [Required]
         public bool IsAllInclusive { get; set; }
