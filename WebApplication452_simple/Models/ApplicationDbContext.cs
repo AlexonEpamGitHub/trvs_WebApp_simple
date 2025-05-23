e257579b5cccfb07d6a4931db3240c899c8203c0
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-23 07:29:05 -0700

    OPSEC: applicationdbcontext_cs--use-secure-db-config

--- WebApplication452_simple/Models/ApplicationDbContext.cs
+++ WebApplication452_simple/Models/ApplicationDbContext.cs
@@ -1,17 +1,30 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace WebApplication452_simple.Models
 {
     public class ApplicationDbContext : DbContext
     {
+        private readonly IConfiguration _configuration;
+
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Hotel> Hotels { get; set; }
         public DbSet<Country> Countries { get; set; }
         public DbSet<Order> Orders { get; set; }
 
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IConfiguration configuration)
             : base(options)
         {
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var connectionString = _configuration.GetConnectionString("DefaultConnection");
+                optionsBuilder.UseSqlServer(connectionString);
+            }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
