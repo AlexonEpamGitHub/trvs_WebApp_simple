408185d82fd29677a35e9763755d2f248a9678c3
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-23 05:18:09 -0700

    OPSEC: context_cs--migrated-to-ef-core

--- WebApplication452_simple/Models/ApplicationDbContext.cs
+++ WebApplication452_simple/Models/ApplicationDbContext.cs
@@ -1,4 +1,4 @@
-﻿using System.Data.Entity;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebApplication452_simple.Models
 {
@@ -9,9 +9,42 @@ namespace WebApplication452_simple.Models
         public DbSet<Country> Countries { get; set; }
         public DbSet<Order> Orders { get; set; }
 
-        public ApplicationDbContext()
-            : base("DefaultConnection")
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configure defensive query handling
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.HasKey(c => c.Id);
+                entity.Property(c => c.Name).IsRequired().HasMaxLength(100);
+            });
+
+            modelBuilder.Entity<Hotel>(entity =>
+            {
+                entity.HasKey(h => h.Id);
+                entity.Property(h => h.Name).IsRequired().HasMaxLength(100);
+                entity.Property(h => h.Location).HasMaxLength(200);
+            });
+
+            modelBuilder.Entity<Country>(entity =>
+            {
+                entity.HasKey(c => c.Id);
+                entity.Property(c => c.Name).IsRequired().HasMaxLength(100);
+            });
+
+            modelBuilder.Entity<Order>(entity =>
+            {
+                entity.HasKey(o => o.Id);
+                entity.Property(o => o.CustomerId).IsRequired();
+                entity.Property(o => o.HotelId).IsRequired();
+                entity.Property(o => o.OrderDate).IsRequired();
+            });
         }
     }
 }
