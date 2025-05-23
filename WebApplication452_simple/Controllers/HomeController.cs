8a714c3ab2542f983377d08f4b82d52f11e32c33
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-23 04:21:28 -0700

    Verification final assured netto-core scoped IAction methods align.

--- WebApplication452_simple/Controllers/HomeController.cs
+++ WebApplication452_simple/Controllers/HomeController.cs
@@ -1,24 +1,38 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace WebApplication452_simple.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class HomeController : Microsoft.AspNetCore.Mvc.Controller
+    public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
         public IActionResult Index()
         {
+            _logger.LogInformation("Accessed Index page.");
             return View();
         }
 
+        [HttpGet("About")]
         public IActionResult About()
         {
+            _logger.LogInformation("Accessed About page.");
             ViewBag.Message = "Your application description page.";
             return View();
         }
 
+        [HttpGet("Contact")]
         public IActionResult Contact()
         {
+            _logger.LogInformation("Accessed Contact page.");
             ViewBag.Message = "Your contact page.";
             return View();
         }
