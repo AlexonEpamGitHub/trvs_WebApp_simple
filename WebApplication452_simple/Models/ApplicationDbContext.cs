70b797ae48382e2f6dd62292294673e1bf78cda9
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-23 04:19:36 -0700

    Migrate ApplicationDbContext to Microsoft.EntityFrameworkCore format for .NET 8 compatibility.

--- WebApplication452_simple/Models/ApplicationDbContext.cs
+++ WebApplication452_simple/Models/ApplicationDbContext.cs
@@ -1,17 +1,77 @@
-﻿using System.Data.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace WebApplication452_simple.Models
 {
     public class ApplicationDbContext : DbContext
     {
+        private readonly IConfiguration _configuration;
+
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Hotel> Hotels { get; set; }
         public DbSet<Country> Countries { get; set; }
         public DbSet<Order> Orders { get; set; }
 
-        public ApplicationDbContext()
-            : base("DefaultConnection")
+        public ApplicationDbContext(IConfiguration configuration)
         {
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = _configuration.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Example of defining relationships and constraints using Fluent API
+
+            // Customer entity configuration
+            modelBuilder.Entity<Customer>()
+                .HasKey(c => c.Id);
+
+            modelBuilder.Entity<Customer>()
+                .Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            // Hotel entity configuration
+            modelBuilder.Entity<Hotel>()
+                .HasKey(h => h.Id);
+
+            modelBuilder.Entity<Hotel>()
+                .Property(h => h.Name)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            modelBuilder.Entity<Hotel>()
+                .HasOne(h => h.Country)
+                .WithMany(c => c.Hotels)
+                .HasForeignKey(h => h.CountryId);
+
+            // Country entity configuration
+            modelBuilder.Entity<Country>()
+                .HasKey(c => c.Id);
+
+            modelBuilder.Entity<Country>()
+                .Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            // Order entity configuration
+            modelBuilder.Entity<Order>()
+                .HasKey(o => o.Id);
+
+            modelBuilder.Entity<Order>()
+                .HasOne(o => o.Customer)
+                .WithMany(c => c.Orders)
+                .HasForeignKey(o => o.CustomerId);
+
+            modelBuilder.Entity<Order>()
+                .HasOne(o => o.Hotel)
+                .WithMany(h => h.Orders)
+                .HasForeignKey(o => o.HotelId);
         }
     }
 }
