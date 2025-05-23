4ccb77ca7f3eb4351c8b52339ee0d4ef26931e23
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-23 04:26:02 -0700

    application-dbcontext--added-entity-framework-core

--- WebApplication452_simple/Models/ApplicationDbContext.cs
+++ WebApplication452_simple/Models/ApplicationDbContext.cs
@@ -1,4 +1,4 @@
-﻿using System.Data.Entity;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebApplication452_simple.Models
 {
@@ -9,9 +9,41 @@ namespace WebApplication452_simple.Models
         public DbSet<Country> Countries { get; set; }
         public DbSet<Order> Orders { get; set; }
 
-        public ApplicationDbContext()
-            : base("DefaultConnection")
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Fluent API configuration for Hotel
+            modelBuilder.Entity<Hotel>(entity =>
+            {
+                entity.HasKey(h => h.Id);
+                entity.Property(h => h.Name)
+                      .IsRequired()
+                      .HasMaxLength(100);
+                entity.Property(h => h.Address)
+                      .HasMaxLength(200);
+                entity.HasOne(h => h.Country)
+                      .WithMany(c => c.Hotels)
+                      .HasForeignKey(h => h.CountryId)
+                      .OnDelete(DeleteBehavior.Cascade);
+            });
+
+            // Fluent API configuration for Country
+            modelBuilder.Entity<Country>(entity =>
+            {
+                entity.HasKey(c => c.Id);
+                entity.Property(c => c.Name)
+                      .IsRequired()
+                      .HasMaxLength(100);
+                entity.HasMany(c => c.Hotels)
+                      .WithOne(h => h.Country)
+                      .HasForeignKey(h => h.CountryId);
+            });
         }
     }
 }
