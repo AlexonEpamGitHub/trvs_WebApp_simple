cb178370a0db390d2d64a5cb1271d44cf096316d
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-23 05:14:08 -0700

    QA: filter-config--added-use-exceptionhandler

--- WebApplication452_simple/App_Start/FilterConfig.cs
+++ WebApplication452_simple/App_Start/FilterConfig.cs
@@ -1,13 +1,38 @@
-﻿using System.Web;
-using System.Web.Mvc;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
 
 namespace WebApplication452_simple
 {
-    public class FilterConfig
+    public class Startup
     {
-        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        public void Configure(IApplicationBuilder app)
         {
-            filters.Add(new HandleErrorAttribute());
+            app.UseExceptionHandler(errorApp =>
+            {
+                errorApp.Run(async context =>
+                {
+                    context.Response.StatusCode = 500;
+                    context.Response.ContentType = "text/html";
+
+                    var feature = context.Features.Get<IExceptionHandlerFeature>();
+                    if (feature != null)
+                    {
+                        var exception = feature.Error;
+
+                        await context.Response.WriteAsync("<html><body>");
+                        await context.Response.WriteAsync("<h1>Something went wrong</h1>");
+                        await context.Response.WriteAsync("<p>An unexpected error occurred. Please try again later.</p>");
+                        await context.Response.WriteAsync("</body></html>");
+                    }
+                });
+            });
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
         }
     }
 }
