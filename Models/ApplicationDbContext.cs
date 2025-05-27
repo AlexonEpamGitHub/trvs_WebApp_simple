1b482b2ae52fdb370644c08e2cbe02479146a802
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-27 09:22:17 -0700

    Update ApplicationDbContext to use Entity Framework Core

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelReservationSystem.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; } = null!;
+        public DbSet<Hotel> Hotels { get; set; } = null!;
+        public DbSet<Country> Countries { get; set; } = null!;
+        public DbSet<Order> Orders { get; set; } = null!;
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+            // Configure entity relationships if needed
+            modelBuilder.Entity<Hotel>()
+                .HasOne(h => h.Country)
+                .WithMany(c => c.Hotels)
+                .HasForeignKey(h => h.CountryId);
+                
+            modelBuilder.Entity<Order>()
+                .HasOne(o => o.Customer)
+                .WithMany(c => c.Orders)
+                .HasForeignKey(o => o.CustomerId);
+                
+            modelBuilder.Entity<Order>()
+                .HasOne(o => o.Hotel)
+                .WithMany()
+                .HasForeignKey(o => o.HotelId);
+        }
+    }
+}
