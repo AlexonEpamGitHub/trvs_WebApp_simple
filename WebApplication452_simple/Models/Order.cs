155ae1a3a6ed8906afc36e7cd71e1ceebaf3db9f
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-27 09:26:44 -0700

    Update Order model for .NET 8 migration
    - Changed namespace from WebApplication452_simple.Models to HotelReservationSystem.Models
    - Marked navigation properties (Customer, Hotel) as nullable with ? suffix
    - Added proper data annotations for validation including Display attributes
    - Used DataType attributes for date and currency properties
    - Removed System.Web using directives
    - Added explicit foreign key properties for Customer and Hotel

--- WebApplication452_simple/Models/Order.cs
+++ WebApplication452_simple/Models/Order.cs
@@ -1,33 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
-using WebApplication452_simple.Models;
 
-namespace WebApplication452_simple.Models
+namespace HotelReservationSystem.Models
 {
     public class Order
     {
         public int Id { get; set; }
-
-        [Required]
-        public Customer Customer { get; set; }
         
         [Required]
-        public Hotel Hotel { get; set; }
-
+        public int CustomerId { get; set; }
+        public Customer? Customer { get; set; }
+        
         [Required]
+        public int HotelId { get; set; }
+        public Hotel? Hotel { get; set; }
+        
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Order Date")]
         public DateTime DateOrdered { get; set; }
 
         [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Check-in Date")]
         public DateTime StartDate { get; set; }
 
         [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Check-out Date")]
         public DateTime EndDate { get; set; }
 
+        [Display(Name = "Number of Days")]
         public int NumberOfDays { get; set; }
 
+        [Display(Name = "Total Price")]
+        [DataType(DataType.Currency)]
         public double FullPrice { get; set; }
     }
 }
