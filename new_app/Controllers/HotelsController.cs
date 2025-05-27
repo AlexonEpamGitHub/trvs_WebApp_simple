7640835ff98126e5dbea201ac0c4a0d8e7c16c29
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-27 02:31:44 -0700

    hotelscontroller--added-crud-actions

---
+++ new_app/Controllers/HotelsController.cs
@@ -0,0 +1,154 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using System.Linq;
+using new_app.Data;
+using new_app.Models;
+
+namespace new_app.Controllers
+{
+    public class HotelsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public HotelsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Hotels
+        public async Task<IActionResult> Index()
+        {
+            var hotels = await _context.Hotels.Include(h => h.Country).ToListAsync();
+            return View(hotels);
+        }
+
+        // GET: Hotels/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var hotel = await _context.Hotels
+                .Include(h => h.Country)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (hotel == null)
+            {
+                return NotFound();
+            }
+
+            return View(hotel);
+        }
+
+        // GET: Hotels/Create
+        public IActionResult Create()
+        {
+            ViewData["CountryId"] = new SelectList(_context.Countries, "Id", "Name");
+            return View();
+        }
+
+        // POST: Hotels/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Address,CountryId")] Hotel hotel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(hotel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CountryId"] = new SelectList(_context.Countries, "Id", "Name", hotel.CountryId);
+            return View(hotel);
+        }
+
+        // GET: Hotels/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var hotel = await _context.Hotels.FindAsync(id);
+            if (hotel == null)
+            {
+                return NotFound();
+            }
+            ViewData["CountryId"] = new SelectList(_context.Countries, "Id", "Name", hotel.CountryId);
+            return View(hotel);
+        }
+
+        // POST: Hotels/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Address,CountryId")] Hotel hotel)
+        {
+            if (id != hotel.Id)
+            {
+                return BadRequest();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(hotel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!HotelExists(hotel.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CountryId"] = new SelectList(_context.Countries, "Id", "Name", hotel.CountryId);
+            return View(hotel);
+        }
+
+        // GET: Hotels/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var hotel = await _context.Hotels
+                .Include(h => h.Country)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (hotel == null)
+            {
+                return NotFound();
+            }
+
+            return View(hotel);
+        }
+
+        // POST: Hotels/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var hotel = await _context.Hotels.FindAsync(id);
+            _context.Hotels.Remove(hotel);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool HotelExists(int id)
+        {
+            return _context.Hotels.Any(e => e.Id == id);
+        }
+    }
+}
