7198f8d6c238e928e0e2d59d96e193c5c9008cc0
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-27 02:32:22 -0700

    applicationdbcontext--initial-ef-core-configurations

---
+++ new_app/Data/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using new_app.Models;
+
+namespace new_app.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configure entity relationships and table settings here
+
+            modelBuilder.Entity<Hotel>()
+                .HasOne(h => h.Country)
+                .WithMany(c => c.Hotels)
+                .HasForeignKey(h => h.CountryId);
+
+            modelBuilder.Entity<Order>()
+                .HasOne(o => o.Customer)
+                .WithMany(c => c.Orders)
+                .HasForeignKey(o => o.CustomerId);
+        }
+    }
+}
