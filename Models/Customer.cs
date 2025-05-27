7de5afa546a88e4a86c97861db8754cb13aaf0e0
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-27 09:12:40 -0700

    Create Customer model for .NET 8

---
+++ Models/Customer.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelReservationSystem.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; } = string.Empty;
+        
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        
+        public ICollection<Order>? Orders { get; set; }
+    }
+}
