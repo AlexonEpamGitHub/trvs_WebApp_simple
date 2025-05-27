d3c41ef20c8ae9f47068b02922f8daee73629972
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-27 05:17:30 -0700

    QA: applicationdbcontext--efcore-modernization

--- WebApplication452_simple/Models/ApplicationDbContext.cs
+++ WebApplication452_simple/Models/ApplicationDbContext.cs
@@ -1,4 +1,4 @@
-﻿using System.Data.Entity;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebApplication452_simple.Models
 {
@@ -9,9 +9,38 @@ namespace WebApplication452_simple.Models
         public DbSet<Country> Countries { get; set; }
         public DbSet<Order> Orders { get; set; }
 
-        public ApplicationDbContext()
-            : base("DefaultConnection")
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.HasKey(c => c.CustomerId); // Example primary key mapping
+                entity.Property(c => c.Name).IsRequired().HasMaxLength(100); // Example property mapping
+            });
+
+            modelBuilder.Entity<Hotel>(entity =>
+            {
+                entity.HasKey(h => h.HotelId); // Example primary key mapping
+                entity.Property(h => h.Name).IsRequired().HasMaxLength(200); // Example property mapping
+            });
+
+            modelBuilder.Entity<Country>(entity =>
+            {
+                entity.HasKey(co => co.CountryId); // Example primary key mapping
+                entity.Property(co => co.Name).IsRequired().HasMaxLength(100); // Example property mapping
+            });
+
+            modelBuilder.Entity<Order>(entity =>
+            {
+                entity.HasKey(o => o.OrderId); // Example primary key mapping
+                entity.Property(o => o.Description).HasMaxLength(500); // Example property mapping
+            });
         }
     }
 }
