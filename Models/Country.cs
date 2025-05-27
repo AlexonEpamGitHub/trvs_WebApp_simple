5c4a8dbac14903f6e076d10704c8ffc5b8653dde
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-27 09:12:25 -0700

    Create Country model for .NET 8

---
+++ Models/Country.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelReservationSystem.Models
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; } = string.Empty;
+        
+        public ICollection<Hotel>? Hotels { get; set; }
+    }
+}
