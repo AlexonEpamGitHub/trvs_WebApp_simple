ab286ce96314a13ed986d4964559968709266c43
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-28 01:44:47 -0700

    QA: Refactored Order model to follow .NET 8 nullability patterns and improved data annotations

--- new_app/HotelReservation/Models/Order.cs
+++ new_app/HotelReservation/Models/Order.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HotelReservation.Models
 {
@@ -7,23 +8,36 @@ namespace HotelReservation.Models
         public int Id { get; set; }
         
         [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Booking Date")]
         public DateTime BookingDate { get; set; }
         
         [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Check-In Date")]
         public DateTime CheckInDate { get; set; }
         
         [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Check-Out Date")]
         public DateTime CheckOutDate { get; set; }
         
         [Required]
         public int CustomerId { get; set; }
+        
+        // Navigation property should be nullable
         public Customer? Customer { get; set; }
         
         [Required]
         public int HotelId { get; set; }
+        
+        // Navigation property should be nullable
         public Hotel? Hotel { get; set; }
         
         [Required]
-        public double TotalPrice { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        [DataType(DataType.Currency)]
+        [Display(Name = "Total Price")]
+        public decimal TotalPrice { get; set; }
     }
 }
