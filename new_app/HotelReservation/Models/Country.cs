8234612444dc142126013b73e50d837a89c44311
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-28 01:43:47 -0700

    QA: Refactored Country model to follow .NET 8 nullability patterns and improved validation

--- new_app/HotelReservation/Models/Country.cs
+++ new_app/HotelReservation/Models/Country.cs
@@ -1,11 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace HotelReservation.Models
 {
     public class Country
     {
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(100)]
         public string Name { get; set; } = string.Empty;
         
-        public List<Hotel> Hotels { get; set; } = new List<Hotel>();
+        // Navigation property for related hotels
+        public ICollection<Hotel> Hotels { get; set; } = new List<Hotel>();
     }
 }
