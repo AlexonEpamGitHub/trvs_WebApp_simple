a1a620f96fb68eab7df54eba7a5adbf4d47d50e2
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-28 01:34:06 -0700

    QA: Update Country model with data annotations and null safety

--- new_app/Models/Country.cs
+++ new_app/Models/Country.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace HotelReservationSystem.Models
 {
     public class Country
     {
         public int Id { get; set; }
+        
+        [Required]
+        [MaxLength(100)]
         public string Name { get; set; } = string.Empty;
     }
 }
