8dd00985edf9c060b327ef91a1689c02cdc2950f
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-28 01:44:13 -0700

    QA: Refactored Hotel model to follow .NET 8 nullability patterns and improved validation with proper data types

--- new_app/HotelReservation/Models/Hotel.cs
+++ new_app/HotelReservation/Models/Hotel.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HotelReservation.Models
 {
@@ -7,9 +8,10 @@ namespace HotelReservation.Models
         public int Id { get; set; }
 
         [Required]
-        [MaxLength(255)]
+        [StringLength(255)]
         public string Name { get; set; } = string.Empty;
 
+        // Navigation property should be nullable with ? operator
         public Country? Country { get; set; }
 
         [Required]
@@ -17,18 +19,22 @@ namespace HotelReservation.Models
         public int CountryId { get; set; }
 
         [Required]
-        [MaxLength(50)]
+        [StringLength(50)]
         public string City { get; set; } = string.Empty;
 
         [Required]
-        [Range(1,5)]
+        [Range(1, 5)]
         public int Stars { get; set; }
 
         [Required]
         [Range(1, 1000)]
-        public double PricePerNight { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        [DataType(DataType.Currency)]
+        [Display(Name = "Price Per Night")]
+        public decimal PricePerNight { get; set; }
 
         [Required]
+        [Display(Name = "All Inclusive")]
         public bool IsAllInclusive { get; set; }
     }
 }
