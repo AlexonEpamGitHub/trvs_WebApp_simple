bca01e784637f46715836af6cd4695229913ef94
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-28 01:26:21 -0700

    Create ApplicationDbContext with EF Core configuration

---
+++ new_app/HotelReservation/Models/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelReservation.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+            // Configure entity relationships and constraints here if needed
+        }
+    }
+}
