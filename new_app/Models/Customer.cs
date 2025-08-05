3363a48120f7500d0ded6cdb1f890c8f22d7f1df
AlexonEpamGitHub <135996907+AlexonEpamGitHub@users.noreply.github.com>
2025-08-05 09:26:24 -0400

    customer-model-migration

--- new_app/Models/Customer.cs
+++ new_app/Models/Customer.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
-namespace WebApplication_Core.Models
+namespace WebApplicationCore.Models
 {
     public class Customer
     {
