553ca7e81031463f6cb56cd74a89a65fccc31330
AlexonEpamGitHub <135996907+AlexonEpamGitHub@users.noreply.github.com>
2025-08-05 09:16:00 -0400

    Create ApplicationDbContext for .NET 8 with EF Core

---
+++ new_app/HotelReservationSystem/Data/ApplicationDbContext.cs
@@ -0,0 +1,42 @@
+using HotelReservationSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelReservationSystem.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configure the relationships between entities
+            modelBuilder.Entity<Hotel>()
+                .HasOne(h => h.Country)
+                .WithMany(c => c.Hotels)
+                .HasForeignKey(h => h.CountryId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Order>()
+                .HasOne(o => o.Customer)
+                .WithMany(c => c.Orders)
+                .HasForeignKey(o => o.CustomerId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Order>()
+                .HasOne(o => o.Hotel)
+                .WithMany(h => h.Orders)
+                .HasForeignKey(o => o.HotelId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
