8e931f2051897c9074184f866b66f1fa283eb5d5
AlexonEpamGitHub <135996907+AlexonEpamGitHub@users.noreply.github.com>
2025-08-05 09:25:37 -0400

    hotel-model-migrated-to-dotnet-core

--- new_app/Models/Hotel.cs
+++ new_app/Models/Hotel.cs
@@ -2,8 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace WebApplication_Core.Models
+namespace WebApplicationCore.Models
 {
     public class Hotel
     {
