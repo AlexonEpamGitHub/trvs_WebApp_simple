dae4ab2455bc0e8412cddb80d3895b797e32cdb5
AlexonEpamGitHub <135996907+AlexonEpamGitHub@users.noreply.github.com>
2025-08-05 09:18:46 -0400

    Add Country model for .NET Core 8 application

---
+++ new_app/Models/Country.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace WebApplication_Core.Models
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        public ICollection<Hotel> Hotels { get; set; }
+    }
+}
