a61f7a2f9735d06954d9649525fd29701298fc58
AlexonEpamGitHub <135996907+AlexonEpamGitHub@users.noreply.github.com>
2025-08-05 09:19:01 -0400

    Add Customer model for .NET Core 8 application

---
+++ new_app/Models/Customer.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication_Core.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+
+        public DateTime? Birthdate { get; set; }
+    }
+}
